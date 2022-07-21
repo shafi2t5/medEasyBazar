e8df11d12ec2be5a119b1dd87f4375ed9a3b469d
shafi2t5 <Shafiul.Islam@brainstation-23.com>
2022-07-21 14:05:03 +0600

    doctor page

--- src/components/layout/layout-doctor.tsx
+++ src/components/layout/layout-doctor.tsx
@@ -12,56 +12,49 @@ const Layout: React.FC = ({ children }) => {
   const { searchList, search_input } = useUI();
   const { data, isLoading, error } = useDepartmentsQuery();
 
-  if (isLoading) {
-    return (
-      <div className="container 2xl:ltr:pr-10 2xl:rtl:pl-10 mt-10 mx-auto">
-        Loading...
-      </div>
-    );
-  }
-
-  if (error) {
-    return (
-      <div className="container 2xl:ltr:pr-10 2xl:rtl:pl-10 mt-10 mx-auto">
-        <Alert message={error?.message} />
-      </div>
-    );
-  }
-
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-
-      <main
-        className="relative flex-grow"
-        style={{
-          WebkitOverflowScrolling: 'touch',
-        }}
-      >
-        <Container className="w-full h-full">
-          <div className={`xl:flex md:pb-2.5 pt-4`}>
-            <div className="hidden xl:block shrink-0 ltr:pr-8 rtl:pl-8 xl:w-[320px] 2xl:w-[370px] pt-px">
-              <div className="bg-brand-sidebarColor flex flex-col justify-between rounded-md">
-                {data?.data?.departments?.map((dept: any, index: any) => (
-                  <DoctorListCard
-                    key={`category--key-${dept.id}`}
-                    dept={dept}
-                    href={'/'}
-                    className="transition"
-                    variant="small"
-                    index={index}
-                  />
-                ))}
+      {isLoading ? (
+        <div className="container 2xl:ltr:pr-10 2xl:rtl:pl-10 mt-10 mx-auto">
+          Loading...
+        </div>
+      ) : error ? (
+        <div className="container 2xl:ltr:pr-10 2xl:rtl:pl-10 mt-10 mx-auto">
+          <Alert message={error?.message} />
+        </div>
+      ) : (
+        <main
+          className="relative flex-grow"
+          style={{
+            WebkitOverflowScrolling: 'touch',
+          }}
+        >
+          <Container className="w-full h-full">
+            <div className={`xl:flex md:pb-2.5 pt-4`}>
+              <div className="hidden xl:block shrink-0 ltr:pr-8 rtl:pl-8 xl:w-[320px] 2xl:w-[370px] pt-px">
+                <div className="bg-brand-sidebarColor flex flex-col justify-between rounded-md">
+                  {data?.data?.departments?.map((dept: any, index: any) => (
+                    <DoctorListCard
+                      key={`category--key-${dept.id}`}
+                      dept={dept}
+                      href={'/'}
+                      className="transition"
+                      variant="small"
+                      index={index}
+                    />
+                  ))}
+                </div>
               </div>
+              {search_input && searchList?.length > 0 ? (
+                <SearchData />
+              ) : (
+                <div className="w-full trendy-main-content">{children}</div>
+              )}
             </div>
-            {search_input && searchList?.length > 0 ? (
-              <SearchData />
-            ) : (
-              <div className="w-full trendy-main-content">{children}</div>
-            )}
-          </div>
-        </Container>
-      </main>
+          </Container>
+        </main>
+      )}
       <Footer />
       <MobileNavigation />
     </div>
